49976a5c7511a187aaa21fa4ef4cfcca1ae3f130
Chelly <67005998+Chelly142@users.noreply.github.com>
2020-08-07 19:48:05 +0900

    Create BFS.py

---
+++ DFS-BFS/BFS.py
@@ -0,0 +1,12 @@
+from collections import deque
+
+def bfs(graph, start, visited):
+    visited[start] = True
+    que = deque([start])
+    while que:
+        v = que.popleft()
+        print(v,end =' ')
+        for i in graph[v]:
+            if not visited[i]:
+                que.append(graph[i])
+                visited[i] =True
