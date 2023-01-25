3e4977c858d3ea39737f12819a62d37f42c9f9f1
Sehyun Im <67005998+Chelly142@users.noreply.github.com>
2023-01-25 18:07:04 +0900

    Update 미로 탈출.py

--- DFS-BFS/미로 탈출.py
+++ DFS-BFS/미로 탈출.py
@@ -25,3 +25,43 @@ def bfs(x,y):
                 graph[nx][ny] = graph[x][y]+1
     return graph[n-1][m-1]
 print(bfs(0,0))
+
+'''
+from collections import deque
+n,m = map(int,input().split())
+graph = []
+for i in range(n):
+  graph.append(list(map(int,input())))
+
+x,y = 0,0
+q = deque()
+q.append((x,y))
+
+while True:
+  if q:
+   v = q.popleft()
+  else:
+    break
+  if v == (n-1,m-1):
+    break
+  nx = v[0]
+  ny = v[1]
+  flag = False
+  if nx+1 < n and graph[nx+1][ny]:
+    q.append((nx+1,ny))
+    graph[nx+1][ny] = graph[nx][ny] + 1
+
+  if ny+1 < m and graph[nx][ny+1]:
+    q.append((nx,ny+1))
+    graph[nx][ny+1] = graph[nx][ny] + 1
+
+  if nx-1 >= 0  and graph[nx-1][ny]:
+    q.append((nx-1,ny))
+    graph[nx-1][ny] = graph[nx][ny] + 1
+
+  if ny-1 >= 0 and graph[nx][ny-1]:
+    q.append((nx,ny-1))
+    graph[nx][ny-1] = graph[nx][ny] + 1
+
+print(graph[n-1][m-1])
+'''
