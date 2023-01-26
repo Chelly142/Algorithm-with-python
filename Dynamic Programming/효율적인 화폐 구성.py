2fb0857e7cc82ea20ba249a650e0ee95161a6f82
Sehyun Im <67005998+Chelly142@users.noreply.github.com>
2023-01-26 17:37:14 +0900

    Update 효율적인 화폐 구성.py

--- Dynamic Programming/효율적인 화폐 구성.py
+++ Dynamic Programming/효율적인 화폐 구성.py
@@ -14,3 +14,26 @@ else:
     print(-1)
 
 #진짜 줜나게 어렵고 다보고 배낀 수준 무줘건 다시 작성해볼것!
+n,m =3,40
+c =[3,5,7]
+d = [10001]*(m+1)
+
+#a(n) = min(a(n-2),a(n-3)) + 1 
+for i in range(min(c),m+1):
+  array =[]
+  if i in c:
+    d[i] = 1
+    continue
+  else:
+    for j in c:
+      if i>=j:
+        array.append(d[i-j])
+
+  d[i] = min(array)+1
+
+
+if d[m]>10000:
+  print(-1)
+else:
+  print(d[m])
+#성장했구나 세현아!
