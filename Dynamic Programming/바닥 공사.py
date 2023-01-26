2a012c739ffec5e84106e4144db8ffee7b65d875
Sehyun Im <67005998+Chelly142@users.noreply.github.com>
2023-01-26 17:05:17 +0900

    Update 바닥 공사.py

--- Dynamic Programming/바닥 공사.py
+++ Dynamic Programming/바닥 공사.py
@@ -7,3 +7,13 @@ for i in range(2,n+1):
     else:
         d[i] = d[i-1]*2+1
 print(d[n])
+
+'''
+n = 4
+d = [0]*(n+1)
+d[1] = 1
+d[2] = 3
+for i in range(3,n+1):
+  d[i] = d[i-2] *2 + d[i-1]
+print(d[n])
+'''
