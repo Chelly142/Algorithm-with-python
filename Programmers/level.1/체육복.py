62e333b0f51a1dbba3599aee2c9dafff593fd4b0
Sehyun Im <67005998+Chelly142@users.noreply.github.com>
2023-01-31 17:40:18 +0900

    Update 체육복.py

--- Programmers/level.1/체육복.py
+++ Programmers/level.1/체육복.py
@@ -16,3 +16,18 @@ def solution(n, lost, reserve):
             continue
     answer = n - len(lost)
     return answer
+
+def solution(n, lost, reserve):
+    l = [1]*(n+2)
+    for i in lost:
+        l[i]-=1
+    for i in reserve:
+        l[i]+=1
+    for i in range(1,n+1):
+        if l[i]==2 and l[i-1]==0:
+            l[i-1]=1
+            l[i]=1
+        elif l[i]==2 and l[i+1]==0:
+            l[i+1]=1
+            l[i]=1
+    return n - l.count(0)
