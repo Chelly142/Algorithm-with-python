5fd9845fd86278d44abbb596541556c6723fc29e
Sehyun Im <67005998+Chelly142@users.noreply.github.com>
2023-02-06 13:58:10 +0900

    Update 럭키 스트레이트.py

--- Implementation/럭키 스트레이트.py
+++ Implementation/럭키 스트레이트.py
@@ -12,3 +12,15 @@ if b == a:
     print("LUCKY")
 else:
     print("READY")
+
+n=input()
+l = len(n)
+left, right =0,0
+for i in n[:int(l/2)]:
+  left+=int(i)
+for i in n[int(l/2):]:
+  right+=int(i)
+if (left-right):
+  print("READY")
+else:
+  print("LUCKY")
