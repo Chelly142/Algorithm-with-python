4933657886c56701c09c9249b28a871593f02056
Sehyun Im <67005998+Chelly142@users.noreply.github.com>
2023-02-08 18:52:03 +0900

    Update 기둥과 보 설치.py

--- Implementation/기둥과 보 설치.py
+++ Implementation/기둥과 보 설치.py
@@ -29,3 +29,42 @@ def solution(n, build_frame):
                 answer.remove([i[0],i[1],1])
     answer.sort()
     return answer
+
+from collections import deque
+# 작업 무시 할지 안할지 판단하는 함수 can
+# 기본적으로 보 기둥 상관없이 디딤이 되는 곳을 저장하는 리스트 check 보 사이 낀 보 조심
+def can(now_build):
+    for i in now_build:
+        x = i[0]
+        y = i[1]
+        if i[2]==0:
+            if y==0 or [x,y-1,0] in now_build or [x,y,1] in now_build or [x-1,y,1] in now_build:
+                continue
+            else:
+
+                return False
+        if i[2]==1:
+            if [x,y-1,0] in now_build or [x+1,y-1,0] in now_build: 
+                continue
+            elif [x-1,y,1] in now_build and [x+1,y,1] in now_build:
+                continue
+            else:
+                return False
+    return True
+
+def solution(n, build_frame):
+    now_build = []
+    bf = deque(build_frame)
+    while bf:
+        work = bf.popleft()
+        w = work[:3]
+        if work[3] == 1:
+            now_build.append(w)
+            if not can(now_build):
+                now_build = now_build[:-1]
+        if work[3] == 0:
+            if w in now_build:
+                now_build.remove(w)
+                if not can(now_build):
+                    now_build.append(w)
+    return sorted(now_build)
