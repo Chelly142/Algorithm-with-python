c5712a70d0232dec069510aa7b597bfe144cd59a
Sehyun Im <tpgus0510@naver.com>
2024-06-28 14:43:56 +0900

    [level 2] Title: 줄 서는 방법, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/12936. 줄 서는 방법/줄 서는 방법.py
+++ 프로그래머스/2/12936. 줄 서는 방법/줄 서는 방법.py
@@ -3,13 +3,9 @@ def solution(n, k):
     answer = []
     l = list(range(1,n+1))
     k-=1
-    def factorial(t):
-        f = 1
-        for i in range(1,t+1):
-            f*=i
-        return f
     for i in range(1,n+1):
-        answer.append(l.pop(k//math.factorial(n-i)))
-        k = k%math.factorial(n-i)
+        fact = math.factorial(n-i)
+        answer.append(l.pop(k//fact))
+        k = k%fact
     
     return answer
