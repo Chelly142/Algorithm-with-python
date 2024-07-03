f7b59593755c3496907acc07714214c7f133c40b
Sehyun Im <tpgus0510@naver.com>
2024-07-03 17:44:10 +0900

    [Silver V] Title: 소트인사이드, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+# n = int(input())
+# n, k= map(int, input().split())
+
+n = input()
+for i in sorted(n,reverse=True):
+    print(i,end="")
+
+
+                    
+
+                        
