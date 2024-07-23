74727c64a31069a763f20fac9ecec6866c4ef20c
Sehyun Im <tpgus0510@naver.com>
2024-07-23 15:45:24 +0900

    [Gold IV] Title: 공유기 설치, Time: 204 ms, Memory: 118268 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.py
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.py
@@ -18,13 +18,6 @@ def is_possible(mid):
             now_house = nxt_house
         if mid_c>=c:
             return True
-    for i in range(n-1,-1,-1):
-        nxt_house = houses[i]
-        if nxt_house-now_house>=mid:
-            mid_c+=1
-            now_house = nxt_house
-        if mid_c>=c:
-            return True
     return False
 
 start ,end = 1,max(houses)
