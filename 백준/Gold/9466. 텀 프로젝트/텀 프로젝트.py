d2e5d5ece7223ac4c3e41e947efcd835e2f3e8dd
Sehyun Im <tpgus0510@naver.com>
2024-08-01 15:39:36 +0900

    [Gold III] Title: 텀 프로젝트, Time: 2904 ms, Memory: 242520 KB -BaekjoonHub

--- 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.py
+++ 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.py
@@ -3,34 +3,34 @@ sys.setrecursionlimit(111111)
 input = sys.stdin.readline
 
 #12:43
-t = int(input())
 
-
-
-
-for i in range(t):
-
-    n = int(input())
-    answer = n
-    prefer_list = list(map(int,input().split()))
-    prefer_dict = {i+1:prefer_list[i] for i in range(n)}
-    visited = set()
-    route =[]
+def solution(n, prefer_list):
+    visited = [False]*(n+1)
+    finished = [False]*(n+1)
 
     def dfs(node):
-        global answer
-        visited.add(node)
-        route.append(node)
-        nxt = prefer_dict[node]
-        if nxt in visited:
-            if nxt in route:
-                answer -= len(route[route.index(nxt):])
-        else:
-            dfs(nxt)
-    
+        nxt = prefer_list[node]
+        if finished[node]:
+            return 0
+        elif visited[node]: #사이클인 경우
+            cycle_size = 1
+            while nxt!=node:
+                cycle_size+=1
+                nxt = prefer_list[nxt]
+            return cycle_size
+        visited[node] = True
+        cycle_size = dfs(nxt)
+        finished[node] = True
+        
+        return cycle_size
+    answer = n
     for i in range(1,n+1):
-        if i not in visited:
-            route =[]
-            dfs(i)
-    print(answer)
-# 13:18 시간초과
+        if not finished[i]:
+            answer -= dfs(i)
+    return answer
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    prefer_list = [0]+list(map(int, input().split()))
+    print(solution(n, prefer_list))
