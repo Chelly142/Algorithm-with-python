7cf683348ae5a2ba80ef0e49f30fb25895546979
임세현 <tpgus0510@naver.com>
2025-05-20 00:02:47 +0900

    [Gold IV] Title: 스카이라인 쉬운거, Time: 124 ms, Memory: 17136 KB -BaekjoonHub

--- 백준/Gold/1863. 스카이라인 쉬운거/스카이라인 쉬운거.java
+++ 백준/Gold/1863. 스카이라인 쉬운거/스카이라인 쉬운거.java
@@ -8,26 +8,21 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		int n = Integer.parseInt(br.readLine());
-		int[] skyline = new int[n];
-		for (int i = 0; i < n; i++) {
-			st = new StringTokenizer(br.readLine());
-			st.nextToken();
-			int y = Integer.parseInt(st.nextToken());
-			skyline[i] = y;
-		}
 		ArrayDeque<Integer> stack = new ArrayDeque<>();
 
 		int answer = 0;
 		for (int i = 0; i < n; i++) {
-			int nowH = skyline[i];
-			if(nowH==0){
-				answer+=stack.size();
+			st = new StringTokenizer(br.readLine());
+			st.nextToken();
+			int nowH = Integer.parseInt(st.nextToken());
+			if (nowH == 0) {
+				answer += stack.size();
 				stack.clear();
 				continue;
 			}
-			
+
 			while (true) {
-				if(stack.isEmpty()) {
+				if (stack.isEmpty()) {
 					stack.add(nowH);
 					break;
 				}
